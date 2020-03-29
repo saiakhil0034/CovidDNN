401e00935531f8d4718484f01d1cf9b2f6262569
saiakhil0034 <saiakhil00.34@gmail.com>
2020-03-28 23:57:34 -0700

    temp commit

--- dataloader.py
+++ dataloader.py
@@ -13,7 +13,7 @@ from PIL import Image
 class CovidDataset(data.Dataset):
     """Custom Covid Dataset compatible with torch.utils.data.DataLoader."""
 
-    def __init__(self, root, json, ids, transform=None):
+    def __init__(self, root, file_path, transform=None):
         """Set the path for images, labels wrapper.
 
         Args:
@@ -22,43 +22,34 @@ class CovidDataset(data.Dataset):
             transform: image transformer.
         """
         self.root = root
-        self.coco = COCO(json)
-        self.ids = ids
+        self.data = pd.read_csv(file_path, sep = " ", header = None )
         self.transform = transform
 
     def __getitem__(self, index):
         """Returns one data pair (image and label)."""
-        coco = self.coco
-        vocab = self.vocab
-        ann_id = self.ids[index]
-        caption = coco.anns[ann_id]['caption']
-        img_id = coco.anns[ann_id]['image_id']
-        path = coco.loadImgs(img_id)[0]['file_name']
+       
+        img_path = self.data.iloc[index,1]
+        label = self.data.iloc[index,2]
+        
 
-        image = Image.open(os.path.join(self.root, path)).convert('RGB')
+        image = Image.open(os.path.join(self.root, img_path)).convert('RGB')
         if self.transform is not None:
             image = self.transform(image)
 
-        caption = []
-        caption.append(vocab('<start>'))
-        caption.extend([vocab(token) for token in tokens])
-        caption.append(vocab('<end>'))
-        target = torch.Tensor(caption)
+        
         return image, target
 
     def __len__(self):
         return len(self.ids)
 
 
-def get_loader(root, json, ids, vocab, transform, batch_size, shuffle, num_workers):
+def get_loader(root, file_path, transform, batch_size, shuffle, num_workers):
     """Returns torch.utils.data.DataLoader for custom covid dataset."""
-    coco = CovidDataset(root=root,
-                        json=json,
-                        ids=ids,
-                        vocab=vocab,
+    covid = CovidDataset(root=root,
+                        file_path=file_path,
                         transform=transform)
 
-    data_loader = torch.utils.data.DataLoader(dataset=coco,
+    data_loader = torch.utils.data.DataLoader(dataset=covid,
                                               batch_size=batch_size,
                                               shuffle=shuffle,
                                               num_workers=num_workers)
---
+++ train.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import nntools as nt
+from models import architectures
+import torch
+import torch.nn as nn
+from config import args
+import os
+from dataloader import get_loader
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+model = architectures.CovidDNN().to(device)
+
+
+model.apply(weights_init)
+
+criterion = args['loss_criterion']
+
+params = list(model.parameters())
+
+optimizer = torch.optim.Adam(params, lr=args['learning_rate'], betas=(args['beta'], 0.999))
+
+stats_manager= nt.StatsManager()
+
+
+exp1 = nt.Experiment(model, device, criterion, optimizer, stats_manager,
+                     output_dir=args['model_path'])
+
+
+if __name__ == "__main__":
+	exp1.run(num_epochs=args['epochs'])
